3209b0a2da9e558a058ebd19b5de52e218dc503a
YassinBenTaher <yassinbentaher@yahoo.com>
2021-07-02 17:31:42 +0100

    updated sdk payment

--- src/Payment/Models/Payment/Domestic/SpDomesticPayment.ts
+++ src/Payment/Models/Payment/Domestic/SpDomesticPayment.ts
@@ -1,13 +1,11 @@
 import {JsonProperty} from 'typescript-json-serializer';
+
 export class SpDomesticPayment {
-    @JsonProperty({name:'amount'})
+    @JsonProperty({name: 'amount'})
     public Amount: number;
     @JsonProperty({name: 'description'})
     public Description: string;
-    @JsonProperty({name: 'successUrl'})
-    public SuccessUrl: string;
-    @JsonProperty({name:'failUrl'})
-    public FailUrl: string;
+
     constructor() {
 
     }
